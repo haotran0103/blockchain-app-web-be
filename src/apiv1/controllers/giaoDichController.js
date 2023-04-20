f1282e3c9c4aaae02bb12eb96fcc1dd3b258d795
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-04-20 07:07:11 +0700

    fix sql

--- src/apiv1/controllers/giaoDichController.js
+++ src/apiv1/controllers/giaoDichController.js
@@ -5,7 +5,7 @@ const db = require("../../configs/configsDatabase");
 module.exports = {
   get: (req, res) => {
     let sql =
-      "SELECT g.*, p.* FROM giaoDich g INNER JOIN project p ON g.idProject = p.id WHERE g.maVi = ?";
+      "SELECT g.*, p.* FROM giaodich g INNER JOIN project p ON g.idProject = p.id WHERE g.maVi = ?";
     db.query(sql, [req.params.maVi], (err, response) => {
       if (err) throw err;
       console.log(response);

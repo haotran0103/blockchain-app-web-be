fa1ae14485a401de1893787d05cdc92e9cdc6edb
haotran0103 <83738152+haotran0103@users.noreply.github.com>
2023-04-23 23:49:34 +0700

    fix return payment

--- src/apiv1/controllers/returnController.js
+++ src/apiv1/controllers/returnController.js
@@ -1,18 +1,30 @@
 module.exports = {
   post: async (req, res) => {
-    const data = req.query;
-    const vnp_SecureHash = data.vnp_SecureHash;
-    delete data.vnp_SecureHashType;
-    delete data.vnp_SecureHash;
-    const signData = querystring.stringify(data, { encode: false });
-    const secureHash = sha256(process.env.VNP_HASH_SECRET + signData);
-    if (vnp_SecureHash === secureHash) {
-      // Thanh toán thành công
-      // Thực hiện xử lý dữ liệu trả về tại đây
-      res.send("Thanh toán thành công!");
-    } else {
-      // Thanh toán thất bại
-      res.send("Thanh toán thất bại!");
-    }
+    var vnp_Params = req.query;
+    
+        var secureHash = vnp_Params['vnp_SecureHash'];
+    
+        delete vnp_Params['vnp_SecureHash'];
+        delete vnp_Params['vnp_SecureHashType'];
+    
+        vnp_Params = sortObject(vnp_Params);
+    
+        var config = require('config');
+        var tmnCode = config.get('vnp_TmnCode');
+        var secretKey = config.get('vnp_HashSecret');
+    
+        var querystring = require('qs');
+        var signData = querystring.stringify(vnp_Params, { encode: false });
+        var crypto = require("crypto");     
+        var hmac = crypto.createHmac("sha512", secretKey);
+        var signed = hmac.update(new Buffer(signData, 'utf-8')).digest("hex");     
+    
+        if(secureHash === signed){
+            //Kiem tra xem du lieu trong db co hop le hay khong va thong bao ket qua
+    
+            res.render('success', {code: vnp_Params['vnp_ResponseCode']})
+        } else{
+            res.render('success', {code: '97'})
+        }
   },
 };
